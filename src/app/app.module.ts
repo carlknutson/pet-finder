3de7a9f770027505dc0e007570294f33a64cc2e5
Carl Knutson <knuts829@umn.edu>
2020-04-04 16:24:08 -0500

    test husky

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,31 +1,31 @@
-import { BrowserModule } from "@angular/platform-browser";
-import { HttpClientModule } from "@angular/common/http";
-import { NgModule } from "@angular/core";
-import { MatSidenavModule, MatSelectModule } from "@angular/material";
-import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
-import { MatCheckboxModule } from "@angular/material/checkbox";
-import { FormsModule } from "@angular/forms";
-import { MatInputModule } from "@angular/material";
-import { Ng4LoadingSpinnerModule } from "ng4-loading-spinner";
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { NgModule } from '@angular/core';
+import { MatSidenavModule, MatSelectModule } from '@angular/material';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { FormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material';
+import { Ng4LoadingSpinnerModule } from 'ng4-loading-spinner';
 
-import { AppRoutingModule } from "./app-routing.module";
-import { AppComponent } from "./app.component";
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
 
 @NgModule({
-	declarations: [AppComponent],
-	imports: [
-		BrowserModule,
-		AppRoutingModule,
-		HttpClientModule,
-		MatSidenavModule,
-		BrowserAnimationsModule,
-		MatCheckboxModule,
-		FormsModule,
-		MatSelectModule,
-		MatInputModule,
-		Ng4LoadingSpinnerModule.forRoot(),
-	],
-	providers: [],
-	bootstrap: [AppComponent],
+  declarations: [AppComponent],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    MatSidenavModule,
+    BrowserAnimationsModule,
+    MatCheckboxModule,
+    FormsModule,
+    MatSelectModule,
+    MatInputModule,
+    Ng4LoadingSpinnerModule.forRoot(),
+  ],
+  providers: [],
+  bootstrap: [AppComponent],
 })
 export class AppModule {}

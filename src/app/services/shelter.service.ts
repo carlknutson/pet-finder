3f09f94ccdb486579b53d7a770f7bc13a4436ad4
Carl Knutson <knuts829@umn.edu>
2020-04-04 19:33:36 -0500

    formatting changes

--- src/app/services/shelter.service.ts
+++ src/app/services/shelter.service.ts
@@ -3,24 +3,23 @@ import { PetfinderApiService } from './petfinder-api.service';
 import { ChromeStorageService } from './chrome-storage.service';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ShelterService {
-
-  constructor(private petFinderApiService: PetfinderApiService,
-              private chromeStorageService: ChromeStorageService) { }
+  constructor(private petFinderApiService: PetfinderApiService, private chromeStorageService: ChromeStorageService) {}
 
   getPets(zip: string, type: string) {
-
     const pets = [];
     return new Promise((resolve, reject) => {
       this.chromeStorageService.getWatchedPets(type).then((watchedPets: any[]) => {
-        this.petFinderApiService.getPets(zip, type, 1).then((petFinderPets: any[]) => {
-          resolve(watchedPets.concat(petFinderPets));
-        },
-        error => {
-        reject();
-        });
+        this.petFinderApiService.getPets(zip, type, 1).then(
+          (petFinderPets: any[]) => {
+            resolve(watchedPets.concat(petFinderPets));
+          },
+          (error) => {
+            reject();
+          }
+        );
       });
     });
   }
